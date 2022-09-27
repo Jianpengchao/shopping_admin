ba2c3a9800477408cc87d828ee60ddb6bd3c7e77
huanglin <2762565371@qq.com>
2022-09-28 00:08:28 +0800

    ✨ feat:完成头部的退出登录

--- src/layout/components/Header.vue
+++ src/layout/components/Header.vue
@@ -1,10 +1,46 @@
 <script setup lang="ts">
+	import { ref } from 'vue'
+	import { useRouter } from "vue-router"
+	import useStore from '../../store'
+	import { ElNotification } from 'element-plus'
 	
+	const { userStore } = useStore()
+	const circleUrl = ref('https://gimg2.baidu.com/image_search/src=http%3A%2F%2Finews.gtimg.com%2Fnewsapp_bt%2F0%2F9680744090%2F1000&refer=http%3A%2F%2Finews.gtimg.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1666881778&t=c6ea51246d22e3b30c87672b884fef74')
+	
+	const router = useRouter()
+
+	const onLogout = async () => {
+		await userStore.$logout()
+
+		router.replace('/login')
+		ElNotification({ title: '温馨提示', message: '退出成功！', type: 'success' })
+
+	}
 </script>
 
 <template>
 	<div class="layout-header">
-		头部
+		<div></div>
+		<div>
+			<el-dropdown>
+				<span class="el-dropdown-link">
+					<el-avatar :size="24" :src="userStore.avatar || circleUrl" />
+					<span class="user-name">{{userStore.username}}</span>
+				</span>
+				<template #dropdown>
+					<el-dropdown-menu>
+						<el-dropdown-item>
+							<el-icon><Icon icon="User" /></el-icon>
+							个人中心
+						</el-dropdown-item>
+						<el-dropdown-item @click="onLogout">
+							<el-icon><Icon icon="Remove" /></el-icon>
+							退出登录
+						</el-dropdown-item>
+					</el-dropdown-menu>
+				</template>
+			</el-dropdown>
+		</div>
 	</div>
 </template>
 
@@ -13,6 +49,23 @@
 	height: 100%;
 	padding: 0 20px;
 	box-sizing: border-box;
-	background-color: antiquewhite;
+	background-color: #fff;
+	border-bottom: 2px solid #eee;
+
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+
+	.el-dropdown-link {
+		line-height: 28px;
+		display: flex;
+		align-items: center;
+		
+		.user-name {
+			margin-left: 8px;
+			font-weight: bold;
+			transform: translateY(2px);
+		}
+	}
 }
 </style>
--- src/layout/index.vue
+++ src/layout/index.vue
@@ -6,10 +6,10 @@
 <template>
     <div class="common-layout">
       <el-container style="height: 100vh;">
-        <el-aside width="210px" style="box-shadow: 8px 0px 10px #eee;z-index: 999;"><Aside /></el-aside>
+        <el-aside width="210px" style="box-shadow: 4px 0px 10px #ccc;z-index: 999;"><Aside /></el-aside>
         <el-container>
           <el-header style="padding: 0;height: 44px;"><Header /></el-header>
-          <el-main>
+          <el-main class="common-layout-main">
             <router-view />
           </el-main>
         </el-container>
@@ -20,5 +20,8 @@
 <style scoped lang="less">
 .common-layout {
   min-height: 100vh;
+  &-main {
+    background-color: #f7f7f7;
+  }
 }
 </style>
